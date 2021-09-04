53be2a343509d20d862f25c2053f1a031c07c576
Luana <luana.nadai.santos@gmail.com>
2021-09-03 21:04:58 -0300

    Solved Lab

---
+++ app.js
@@ -0,0 +1,20 @@
+const { request } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (request, response) => {
+    response.sendFile(__dirname + '/views/works.html')
+})
+
+app.listen(3000, () => console.log('Servidor on na porta 3000'))
